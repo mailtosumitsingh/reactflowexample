f835690956055f526e840600198ea94a834373de
mailtosumitsingh <mailtosumitsingh@gmail.com>
2023-05-20 12:53:15 -0400

    Added delete conn support

---
+++ src/DownloadButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Panel } from 'reactflow';
+import { toPng } from 'html-to-image';
+
+function downloadImage(dataUrl) {
+  const a = document.createElement('a');
+
+  a.setAttribute('download', 'reactflow.png');
+  a.setAttribute('href', dataUrl);
+  a.click();
+}
+
+function DownloadButton() {
+  const onClick = () => {
+    toPng(document.querySelector('.react-flow'), {
+      filter: (node) => {
+        // we don't want to add the minimap and the controls to the image
+        if (
+          node?.classList?.contains('react-flow__minimap') ||
+          node?.classList?.contains('react-flow__controls')
+        ) {
+          return false;
+        }
+
+        return true;
+      },
+    }).then(downloadImage);
+  };
+
+  return (
+    <Panel position="top-left">
+      <button className="download-btn" onClick={onClick}>
+        Download Image
+      </button>
+    </Panel>
+  );
+}
+
+export default DownloadButton;
